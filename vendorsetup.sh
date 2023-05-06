125c16bf893a15b04b4d949a24dc2ab2c3098f0d
Shravankumar <shravankumarsayz@gmail.com>
2023-05-06 11:25:03 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,23 +1,21 @@
 # Vendor Tree
-echo 'Cloning Vendor tree [1/6]'
-git clone git@github.com:shravansayz/vendor_realme_RMX1901_13.git -b 13 vendor/realme/RMX1901
+echo 'Cloning Vendor tree [1/5]'
+git clone git@github.com:H1mJT/vendor_realme_RMX1901_13.git vendor/realme/RMX1901
 
-# Kernel Tree
-echo 'Cloning Kernel tree [2/6]'
-git clone git@github.com:shravansayz/kernel_realme_RMX1901.git -b x.319 kernel/realme/RMX1901
+#Kernel tree
+echo 'Cloning Kernel tree [2/5]'
+git clone --recurse-submodules git@github.com:H1mJT/kernel_realme_RMX1901.git -b ksu kernel/realme/RMX1901
 
-# Realme In-Screen Proximity Patch
-echo 'Patching Realme In-Screen Proximity [3/6]'
-cd frameworks/base && git fetch git@github.com:ashim-anwar/proximity_patch.git 13 && git cherry-pick 30ea88924980002d94fb8e2bf4a5016905ca6914 && cd ../..
+#Syncing KernelSU to latest source
+echo "Syncing KernelSU to latest source [3/5]"
+cd kernel/realme/RMX1901 && git submodule sync && git submodule update --init --recursive && cd KernelSU && git checkout main && git pull && cd ../../../..
 
 # Use Proton Clang For Compilation
-echo ' Cloning Proton Clang For Compilation [4/6]'
+echo ' Using Proton Clang For Compilation [4/5]'
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
 
-# Inherit Pocket Mode
-echo ' Cloning PocketMode [5/6]'
-git clone git@github.com:ashim-anwar/packages_apps_PocketMode.git -b 13 packages/apps/PocketMode
 
-# Inherit ViPER4AndroidFX
-echo ' Cloning ViPER4AndroidFX [6/6]'
-git clone git@github.com:ashim-anwar/packages_apps_ViPER4AndroidFX.git -b master packages/apps/ViPER4AndroidFX
+echo 'Patching Realme In-Screen Proximity [5/5]'
+# Realme In-Screen Proximity Patch
+cd frameworks/base && git fetch git@github.com:H1mJT/proximity_patch.git && git cherry-pick afb38a0f9f9a612b13b12b901ab9b7f0fffae3de && cd ../..
+
