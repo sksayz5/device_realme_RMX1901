ed178da60843c5df7c252d2532decaada14da6ca
Shravankumar <shravankumarsayz@gmail.com>
2023-05-07 19:44:13 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1901.mk
+    $(LOCAL_DIR)/cherish_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_RMX1901-userdebug \
-    aosp_RMX1901-eng
+    cherish_RMX1901-userdebug \
+    cherish_RMX1901-eng
 
