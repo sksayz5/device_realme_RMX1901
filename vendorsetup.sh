9eb6d0d7a95e3270573543a13ec1e5fd02e464fd
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-06-27 11:53:27 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,3 @@
-# Vendor Tree
-echo 'Cloning Vendor tree [1/4]'
-git clone git@github.com:H1mJT/vendor_realme_RMX1901_13.git vendor/realme/RMX1901
-
-#Kernel tree
-echo 'Cloning Kernel tree [2/4]'
-git clone --recurse-submodules git@github.com:shravansayz/kernel_realme_RMX1901.git -b x.337_ksu kernel/realme/RMX1901
-
 #Syncing KernelSU to latest source and avoid a conflict
-echo "Syncing KernelSU to latest source [3/4]"
+echo "Syncing KernelSU to latest source [1/1]"
 cd kernel/realme/RMX1901 && git submodule sync && git submodule update --init --recursive && cd KernelSU && git checkout main && git pull && touch Android.mk && cd ../../../..
-
-echo 'Patching Realme In-Screen Proximity [4/4]'
-# Realme In-Screen Proximity Patch
-cd frameworks/base && git fetch git@github.com:H1mJT/proximity_patch.git && git cherry-pick afb38a0f9f9a612b13b12b901ab9b7f0fffae3de && cd ../..
-
