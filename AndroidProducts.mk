26c470eca542f9337070dc5d153f36dd16229cf5
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-06-27 15:06:25 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pixys_RMX1901.mk
+    $(LOCAL_DIR)/octavi_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    pixys_RMX1901-userdebug \
-    pixys_RMX1901-eng
+    octavi_RMX1901-userdebug \
+    octavi_RMX1901-eng
 
